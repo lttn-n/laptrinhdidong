db4a6deb19003bf4026c0739e78d9d46bb2dad81
Lam Thi Thanh Nhi <lamthithanhnhi2006@gmail.com>
2026-08-29 00:37:03 +0700

    Xoa folder btvn_bai01 vi up nham file

--- Tuan 02/btvn_bai01/bin/btvn_bai01.dart
+++
@@ -1,5 +0,0 @@
-import 'package:btvn_bai01/btvn_bai01.dart' as btvn_bai01;
-
-void main(List<String> arguments) {
-  print('Hello world: ${btvn_bai01.calculate()}!');
-}
--- Tuan 02/btvn_bai01/test/btvn_bai01_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:btvn_bai01/btvn_bai01.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
